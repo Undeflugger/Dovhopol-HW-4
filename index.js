3d360e11a320b8ed6e80dde4915902b963ce2d79
Undeflugger <104163210+Undeflugger@users.noreply.github.com>
2022-12-12 17:52:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,30 @@ while (myNum < 21){
         result += `${myNum}, `;
     }
     myNum++;
-} console.log(result);
+} 
+console.log(result);
 console.groupEnd();
 
 console.group('Вивести квадрати чисел від 10 до 20.');
 for(let i=10; i<21; i++){
     console.log(`Квадрат числа ${i} = ${i**2}`);
-}console.groupEnd();
+}
+console.groupEnd();
 
 console.group('Вивести таблицю множення на 7');
 let MyNumber = 1;
-while (MyNumber <=10){
-    console.log(`7 * ${MyNumber} = ${7 * MyNumber}`);
-    MyNumber++;
-}console.groupEnd();
+while (myNumber <=10){
+    console.log(`7 * ${myNumber} = ${7 * myNumber}`);
+    myNumber++;
+}
+console.groupEnd();
 
 console.group('Знайти добуток усіх цілих чисел від 15 до 35');
 let myResult = 1;
 for(let number = 15; number <= 35; number++){
     myResult *= number;
-} console.log(`Добуток цілих чисел від 15 до 35 - ${myResult}`);
+} 
+console.log(`Добуток цілих чисел від 15 до 35 - ${myResult}`);
 console.groupEnd();
 
 console.group('Знайти середнє арифметичне всіх цілих чисел від 1 до 500');
@@ -36,14 +40,16 @@ let mySumm = 0;
 while (value <= 500){
     mySumm += value;
     value++;
-} console.log(`Середнє арифметичне цілих чисел від 1 до 500 - ${mySumm / 500}`);
+} 
+console.log(`Середнє арифметичне цілих чисел від 1 до 500 - ${mySumm / 500}`);
 console.groupEnd();
 
 console.group('Вивести суму лише парних чисел в діапазоні від 30 до 80');
 let p = 0;
 for(n = 30; n<=80; n++){
-    if(n % 2){
+    if(n % 2 === 0){
       p += n;
     }
-} console.log(`Сума парних чисел в діапазоні від 30 до 80 - ${p}`);
+} 
+console.log(`Сума парних чисел в діапазоні від 30 до 80 - ${p}`);
 console.groupEnd();
